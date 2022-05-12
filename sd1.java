403741e26075ba6fede1714ff2244645f2798429
Santiago-Duque <sduque@ucsd.edu>
2022-05-11 23:30:12 -0700

    added test file

--- sd1.java
+++ sd1.java
@@ -1,3 +1,5 @@
 public class sd1 {
-
+    public static int divide(int x, int y) {
+        return x/y;
+    }
 }
---
+++ sd1test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class sd1Test {
+    @Test
+    public void division() {
+        assertEquals(3, sd1.divide(8,3));
+    }
+}
