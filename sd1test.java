b0fc4f5b8e56b97103f05109d5245dc8d063cff6
Santiago-Duque <sduque@ucsd.edu>
2022-05-11 23:37:07 -0700

    Update sd1test.java

--- sd1test.java
+++ sd1test.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
-public class sd1test {
+class sd1test {
     @Test
     public void division() {
         assertEquals(3, sd1.divide(8,3));
